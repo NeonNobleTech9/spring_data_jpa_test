5b411e11aed09b4fdadd0f7e0d47ada8652ea395
unknown <you@example.com>
2019-11-11 13:02:14 +0100

    Added spring initializr

---
+++ src/main/java/com/github/nobletech9/springdatajpa/SpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.nobletech9.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaApplication.class, args);
+	}
+
+}
